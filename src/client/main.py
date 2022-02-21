4dff6b66e5715c4c52dbb1cfe49002dd657b0bd9
Matheus Levi Aidano <matheuslevi.aidano@gmail.com>
2022-02-21 11:47:33 -0300

    Atualiza detalhes visuais do jogo

--- src/client/main.py
+++ src/client/main.py
@@ -1,12 +1,22 @@
-"""Arquivo principal do jogo, contendo a lógica do jogo"""
+"""Arquivo principal, contendo a lógica do jogo"""
 
 import pygame
 
-def handle_play(tile, board, x, y, screen):
+def handle_play(board, x, y, placement_board):
+    result = 'errou'
+    # Se a jogada acertou um alvo
     if board[x][y]['type'] == 'ship':
+        result = 'acertou'
         id = board[x][y]['id']
         for i in range(10):
             for j in range(10):
                 if board[i][j]['id'] == id:
                     board[i][j]['shot'] = 1
-    return board
+                    placement_board[i][j]['shot'] = 1
+                    placement_board[i][j]['type'] = 'ship'
+
+    else:
+        board[x][y]['shot'] = 1
+        placement_board[x][y]['shot'] = 1          
+    
+    return placement_board, board, result
--- src/client/render.py
+++ src/client/render.py
@@ -1,13 +1,15 @@
 """Renderiza o jogo utilizando pygame"""
 import pygame
-from board import create_board
+
+from board import create_board, create_placement_board
 from draw import *
 from main import *
-import time
+from client import Socket, connect_player, get_turn, get_score
 
 WIDTH = 650
 HEIGHT = 650
 SCREENSIZE = (WIDTH, HEIGHT)
+placement_board = create_placement_board() # Tabuleiro em branco que aparecerá na tela do jogador
 board = create_board() # Criando o tabuleiro do jogador
 
 # Iniciado o pygame
@@ -15,16 +17,14 @@ pygame.init()
 screen = pygame.display.set_mode(SCREENSIZE, 0, 32)
 pygame.display.set_caption("Batalha Naval Online")
 
-
-# If you want to draw another instruction you have to fill the text, blit it and then write another one.
-text = draw_instruction('Escolha a posição dos seus navios:', (0,100,0))
-screen.blit(text, (0, 0))
-text.fill((100,100,100))
-screen.blit(text, (0, 0))
-pygame.display.update()
 #-----------------------------------------------------------------------------------------------------------
 
 page = 'home'
+player = 99 # Neste ponto ainda não está definido se este jogador será 1 ou 2
+turn = 1
+cliente = None
+won = 0
+
 draw_homepage(screen)
 pygame.display.update()
 
@@ -39,26 +39,51 @@ while True:
             start_button = pygame.Rect(180, 426, 265, 80)
             if e.type == pygame.MOUSEBUTTONDOWN:
                 if start_button.collidepoint(pygame.mouse.get_pos()):
+                    cliente = Socket()      
                     page = 'wait'
         
-        if page == 'wait':
-            draw_wait(screen)
-            pygame.display.update()
-            # Request pro servidor perguntando a quantidade de jogadores
-            players = 2 #INSERIR REQUEST
-            page = 'board'
-            time.sleep(15)
-            board_buttons = draw_board(screen, board)
-        
-        if page == 'board':
+        elif page == 'board':
+            if turn == player:
+                draw_text("Seu turno", (0, 100, 0), screen)
+            else:
+                draw_text("Turno do oponente", (100, 0, 0), screen)
+            played = 0
             if e.type == pygame.MOUSEBUTTONDOWN:
                 for i in range(10):
                     for j in range(10):
                         button = board_buttons[i][j]
                         if button.collidepoint(pygame.mouse.get_pos()):
-                            # Se for o turno dele, jogar!
-                            board = handle_play(button, board, i, j, screen)
-                            draw_board(screen, board)
+                            turn = get_turn(cliente)
+                            if turn == player:
+                                # Enviando a jogada para o servidor
+                                placement_board, board, result = handle_play(board, i, j, placement_board)
+                                cliente.send_to_server(f"play {player} {result} {i} {j}")
+                                played = 1
+            # Verificando se houve alguma jogada
+            if not played:
+                current = turn
+                turn = get_turn(cliente)
+                if current != turn:
+                    draw_board(screen, placement_board)
+                    score = get_score(cliente)
+                    if score[0] == "winner":
+                        if int(score[1]) == player:
+                            won = 1
+                        page = 'end'
+                    draw_score(screen, score)
+                
 
+        elif page == 'wait':
+            if player == 99:
+                draw_wait(screen)
+                pygame.display.update()
+            # Jogador 1 ficará na função connect_player até o jogador 2 se conectar
+            player = connect_player(cliente)
+            page = 'board'
+            board_buttons = draw_board(screen, placement_board)
+        
+        elif page == 'end':
+            draw_end(screen, won)
+        
         pygame.display.update()
         
